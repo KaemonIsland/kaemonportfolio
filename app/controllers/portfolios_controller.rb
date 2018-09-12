221605926b73c6881abf26154a2606a87ec90c8e
KaemonL <kaemonlovendahl14@hotmail.com>
2018-09-12 17:05:49 -0600

    Implemented destroy action for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,6 +39,10 @@ class PortfoliosController < ApplicationController
 end
 
   def destroy
+    Portfolio.find(params[:id]).destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: "Portfolio was deleted." }
+    end
   end
 
   private
