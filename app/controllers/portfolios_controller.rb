0201840faa1a9f94e8c12778465684ca7344ceea
KaemonL <kaemonlovendahl14@hotmail.com>
2018-09-11 19:19:27 -0600

    Integrated new and create portfolio methods

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,12 +7,22 @@ class PortfoliosController < ApplicationController
   end
 
   def new
+    @portfolio_item = Portfolio.new
   end
 
   def edit
   end
 
   def create
+    @portfolio_item = Portfolio.create!(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: "Your portfolio is now live." }
+      else
+        render 'new'
+      end
+    end
   end
 
   def update
@@ -20,4 +30,9 @@ class PortfoliosController < ApplicationController
 
   def destroy
   end
+
+  private
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
 end
