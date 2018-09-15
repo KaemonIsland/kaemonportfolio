4a851458bd34c8f49d2838a277ec0c9a68e853af
KaemonL <kaemonlovendahl14@hotmail.com>
2018-09-15 11:33:46 -0600

    Added custom method for updating post status

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   resources :portfolios, except: [:show]
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 end
